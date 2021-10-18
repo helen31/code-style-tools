000f048d53cf65b62cf7f42108dc36bcef8596fa
Olena_Hlukhovska <Olena_Hlukhovska@epam.com>
2021-10-18 17:57:25 +0300

    Add eslint, set eslint rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from 'rxjs';
+import {IUserNameInterface} from './models/user-name';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'code-style-tools';
+export class AppComponent implements OnInit{
+  title = 'config esLint';
+  namesList!: IUserNameInterface[];
+  isVisible$ = of(false);
+  protected _dogName = 'Pipa';
+  private _catName = 'Murka';
+  public constructor() {
+  }
+
+  getUser() {
+    var b: string;
+    let obj = {a: 1, c: 3, b: 2};
+    const anotherValue = 27;
+    const x: string[] = ['a', 'b'];
+    b = typeof anotherValue === 'number' ? 'yes': 'no';
+  }
+
+  ngOnInit(): void {
+    console.warn(1);
+  }
+
+
 }
---
+++ src/app/models/user-name.ts
@@ -0,0 +1,11 @@
+export interface IUserNameInterface {
+  name: string;
+}
+
+export type UserNames = 'Danulo' | 'Ivan';
+
+// export default function () {
+//
+// };
+
+export const setUserToken= () => {return 'test'};
